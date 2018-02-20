4ffc3809228f78e05f792c5fcf5e1078293b3dc8
Isabelle <isabellelidberg@live.se>
2018-02-20 23:15:31 +0100

    More small changes to test

--- features/step_definitions/signup_steps.rb
+++ features/step_definitions/signup_steps.rb
@@ -9,15 +9,10 @@ end
 Then("He fills in {string} with {string}") do |string, string2|
   fill_in 'Name', :with => 'Fleur'
 end
-#
-# Then("he fills in {string} with {string}") do |string, string2|
-#  fill_in "email", :with "wookie@email.com"
-# end
 
-
-# Then("he fills in {string} with {string}") do |field, input|
-#   fill_in(field, :with => input)
-# end
+Then("he fills in {string} with {string}") do |string, string2|
+  pending # Write code here that turns the phrase above into concrete actions
+end
 
 Then("He clicks {string}") do |string|
   click_on(string)
